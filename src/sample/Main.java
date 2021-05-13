12a6d2960c2c0240e2649938a021535758a8b580
xVllad <vladbalan002@yahoo.com>
2021-05-14 00:16:14 +0300

    Login.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,9 +13,9 @@ public class Main extends Application {
     //
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1600, 1000));
         primaryStage.show();
     }
 
