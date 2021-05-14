e4a3c995371c528c342866f070ebaf304b4d8aa8
xVllad <vladbalan002@yahoo.com>
2021-05-14 22:50:50 +0300

    STUFF

--- src/sample/View/Login.java
+++ src/sample/View/Login.java
@@ -1,4 +1,27 @@
 package sample.View;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Login {
+
+
+    @FXML private Button Login;
+
+    public void OnClickEvent(MouseEvent mouseEvent) throws IOException {
+
+        Parent RegisterSceneParent = FXMLLoader.load(getClass().getResource("View/Login.fxml"));
+        Scene RegisterScene = new Scene(RegisterSceneParent);
+        Stage window = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(RegisterScene);
+        window.show();
+    }
 }
--- src/sample/View/UserControl.java
+++ src/sample/View/UserControl.java
@@ -1,4 +1,11 @@
 package sample.View;
 
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class UserControl {
+    public void OnClickEv(MouseEvent mouseEvent) throws IOException {
+
+    }
 }
